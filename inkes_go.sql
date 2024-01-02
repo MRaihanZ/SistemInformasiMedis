287f3d03d4e0002c1e6f500e395acde76673d7db
MRaihanZ <mraihanzhafran.14@gmail.com>
2024-01-02 20:38:59 +0700

    change backup database

--- inkes_go.sql
+++ inkes_go.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 01:04 AM
+-- Generation Time: Jan 02, 2024 at 02:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -39,7 +39,33 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(7, 'kopi', '2023-12-04 10:11:40', '2023-12-04 10:11:40');
+(1, 'poli umum', '2024-01-02 10:47:30', '2024-01-02 10:47:30'),
+(2, 'poli gigi', '2024-01-02 10:47:30', '2024-01-02 10:47:30'),
+(3, 'poli kandungan', '2024-01-02 10:47:55', '2024-01-02 10:48:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `name`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 'Raihan', 1, '2024-01-02 10:49:56', '2024-01-02 10:49:56'),
+(2, 'Zaky', 2, '2024-01-02 05:24:16', '2024-01-02 05:24:16'),
+(3, 'Rei', 1, '2024-01-02 05:43:46', '2024-01-02 06:11:20'),
+(4, 'Zhafran', 1, '2024-01-02 05:46:13', '2024-01-02 12:46:50');
 
 -- --------------------------------------------------------
 
@@ -62,7 +88,10 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `category_id`, `stock`, `description`, `created_at`, `updated_at`) VALUES
-(7, 'kapal api', 7, 10, 'mantap', '2023-12-04 10:12:06', '2023-12-04 10:12:06');
+(7, 'kapal api', 7, 10, 'mantap', '2023-12-04 10:12:06', '2023-12-04 10:12:06'),
+(8, 'asdfadsf', 1, 0, NULL, '2024-01-02 05:18:59', '2024-01-02 05:18:59'),
+(9, 'test', 1, 0, NULL, '2024-01-02 05:19:09', '2024-01-02 05:19:09'),
+(10, 'test', 1, 0, NULL, '2024-01-02 05:23:36', '2024-01-02 05:23:36');
 
 --
 -- Indexes for dumped tables
@@ -72,6 +101,12 @@ INSERT INTO `products` (`id`, `name`, `category_id`, `stock`, `description`, `cr
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,13 +123,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
